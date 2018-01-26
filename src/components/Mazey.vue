dfb85cb5f8242f0afb86406b9456dff7fdce26bf
mazey <mazey@mazey.net>
2018-01-26 23:00:47 +0800

    install vue-aplayer to play music.

--- src/components/Mazey.vue
+++ src/components/Mazey.vue
@@ -10,7 +10,7 @@
     name: 'v-mazey',
     data () {
       return {
-        msg: 'v-mazey'
+        msg: 'vv-mazey'
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Mazey from './Mazey'
+import Mazey from './components/Mazey'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Aplayer from 'vue-aplayer'
 
-Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 let MazeyBlog = new Vue({
   el: '#mazey',
   router,
   template: '<Mazey/>',
-  components: { Mazey }
+  components: {
+    Mazey,
+    Aplayer
+  }
 })
 
 Vue.use(MazeyBlog)
