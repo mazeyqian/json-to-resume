29074e3f81d92d1ae72ed8cc9dc67ceb8403dc7f
mazey <mazey@mazey.net>
2018-01-30 23:35:11 +0800

    嵌套完成，解决小屏下拉框透明的问题，小屏时不该固定nav的高度。

--- src/components/page/home.vue
+++ src/components/page/home.vue
@@ -1,11 +1,16 @@
 <template>
   <main class="main">
     <div class="row">
-      <div class="col-lg-6 offset-lg-1">
-        轮播
-      </div>
-      <div class="col-lg-4">
-        热门文章
+      <div class="col-lg-10 offset-lg-1">
+        <div class="row">
+          <div class="col-lg-8">轮播</div>
+          <div class="col-lg-4">心情</div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4">文章</div>
+          <div class="col-lg-4">文章</div>
+          <div class="col-lg-4">文章</div>
+        </div>
       </div>
     </div>
   </main>
