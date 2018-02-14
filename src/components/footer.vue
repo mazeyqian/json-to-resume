9b6d1bba2cd52deffa210f47d8de852edfabe106
mazey <mazey@mazey.net>
2018-02-14 20:42:52 +0800

    完成： 底部框架搭好
    未完成：
    - 布局 前端 / js 前端 / css 后端 / php 数据库 / sql
    - banner 第一个直接获取，之后用异步base64
    - 吃西瓜尺寸不够不显示
    - 标签云美化拉伸条
    - 文章标题列表添加pop显示整个标题
    - 底部版权

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,6 +1,15 @@
 <template>
-  <footer>
-    底部
+  <footer class="row mazey-footer">
+    <div class="col-lg-2 footer-cat" v-for="(item, index) in new Array(5)" :class="{'offset-lg-1': index === 0}">
+      <div class="footer-cat-title">
+        关于我们 {{ index }}
+      </div>
+      <ul class="footer-cat-list">
+        <li v-for="(item, index) in new Array(8)">
+          <a href="#">关于我们</a>
+        </li>
+      </ul>
+    </div>
   </footer>
 </template>
 
