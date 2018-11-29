34416ef95134d42103694a78d8dfb0c2153de2b9
mazey <mazey@mazey.net>
2018-11-29 14:56:59 +0800

    修改简历部分，和跟节点解耦。

--- src/components/m-resume.vue
+++ src/components/m-resume.vue
@@ -1,130 +1,131 @@
 <template>
-  <div class="m-resume">
-    <div class="resume-title">
-      <h1>前端工程师</h1>
-    </div>
-    <!--基本信息-->
-    <div class="resume-item">
-      <h3>基本信息</h3>
-      <div class="resume-item-content resume-item-between">
-        <ul>
-          <li><span class="resume-info-name">姓名：</span>钱程</li>
-          <li><span class="resume-info-name">性别：</span>男</li>
-          <li><span class="resume-info-name">生日：</span>1993 年 4 月</li>
-          <li><span class="resume-info-name">电话：</span>18551470413</li>
-          <li><span class="resume-info-name">邮箱：</span>mazey@mazey.net</li>
-          <li><span class="resume-info-name">工作年限：</span>2年</li>
-        </ul>
-        <ul>
-          <li><span class="resume-info-name">院校：</span>三江学院<span class="resume-small">（2012 年 9 月 - 2016 年 6 月）</span></li>
-          <li><span class="resume-info-name">专业：</span>电气工程及其自动化</li>
-          <li><span class="resume-info-name">学历：</span>本科</li>
-          <li><span class="resume-info-name">英语：</span>CET-6</li>
-          <li><span class="resume-info-name">博客：</span><a href="https://blog.mazey.net/" target="_blank">blog.mazey.net</a></li>
-          <li><span class="resume-info-name">GitHub：</span><a href="https://github.com/mazeyqian" target="_blank">mazeyqian</a></li>
-        </ul>
+  <div class="m-resume-box">
+    <div class="m-resume">
+      <div class="resume-title">
+        <h1>前端工程师</h1>
       </div>
-    </div>
-    <!--工作经历-->
-    <div class="resume-item">
-      <h3>工作经历</h3>
-      <!--Pinnacle-->
-      <div class="resume-experience">
-        <div class="resume-experience-timeline">
-          <div class="timeline-line-before"></div>
-          <div class="timeline-circle"></div>
-          <div class="timeline-line"></div>
+      <!--基本信息-->
+      <div class="resume-item">
+        <h3>基本信息</h3>
+        <div class="resume-item-content resume-item-between">
+          <ul>
+            <li><span class="resume-info-name">姓名：</span>钱程</li>
+            <li><span class="resume-info-name">性别：</span>男</li>
+            <li><span class="resume-info-name">生日：</span>1993 年 4 月</li>
+            <li><span class="resume-info-name">电话：</span>18551470413</li>
+            <li><span class="resume-info-name">邮箱：</span>mazey@mazey.net</li>
+            <li><span class="resume-info-name">工作年限：</span>2年</li>
+          </ul>
+          <ul>
+            <li><span class="resume-info-name">院校：</span>三江学院<span class="resume-small">（2012 年 9 月 - 2016 年 6 月）</span></li>
+            <li><span class="resume-info-name">专业：</span>电气工程及其自动化</li>
+            <li><span class="resume-info-name">学历：</span>本科</li>
+            <li><span class="resume-info-name">英语：</span>CET-6</li>
+            <li><span class="resume-info-name">博客：</span><a href="https://blog.mazey.net/" target="_blank">blog.mazey.net</a></li>
+            <li><span class="resume-info-name">GitHub：</span><a href="https://github.com/mazeyqian" target="_blank">mazeyqian</a></li>
+          </ul>
         </div>
-        <div class="resume-experience-content">
-          <div class="content-title">
-            <div class="company">
+      </div>
+      <!--工作经历-->
+      <div class="resume-item">
+        <h3>工作经历</h3>
+        <!--Pinnacle-->
+        <div class="resume-experience">
+          <div class="resume-experience-timeline">
+            <div class="timeline-line-before"></div>
+            <div class="timeline-circle"></div>
+            <div class="timeline-line"></div>
+          </div>
+          <div class="resume-experience-content">
+            <div class="content-title">
+              <div class="company">
               <span class="company-name">
                 <div class="company-logo-pinnacle">
                   <i class="icon-company"></i>
                 </div>
                 <span>平纳科信息咨询有限公司</span>
               </span>
-              <span>2018 年 7 月 - 至今</span>
+                <span>2018 年 7 月 - 至今</span>
+              </div>
+              <div class="department">
+                <span>技术部</span>
+                <span>前端工程师</span>
+              </div>
             </div>
-            <div class="department">
-              <span>技术部</span>
-              <span>前端工程师</span>
+            <div class="content-description">
+              <span>负责公司猎头系统的前端开发，开发并完善工作流平台；承担前端项目管理和运维的工作。</span>
             </div>
           </div>
-          <div class="content-description">
-            <span>负责公司猎头系统的前端开发，开发并完善工作流平台；承担前端项目管理和运维的工作。</span>
+        </div>
+        <!--苏宁云商-->
+        <div class="resume-experience">
+          <div class="resume-experience-timeline">
+            <div class="timeline-line-before"></div>
+            <div class="timeline-circle"></div>
+            <div class="timeline-line"></div>
           </div>
-        </div>
-      </div>
-      <!--苏宁云商-->
-      <div class="resume-experience">
-        <div class="resume-experience-timeline">
-          <div class="timeline-line-before"></div>
-          <div class="timeline-circle"></div>
-          <div class="timeline-line"></div>
-        </div>
-        <div class="resume-experience-content">
-          <div class="content-title">
-            <div class="company">
+          <div class="resume-experience-content">
+            <div class="content-title">
+              <div class="company">
               <span class="company-name">
                 <div class="company-logo-suning">
                   <i class="icon-company"></i>
                 </div>
                 <span>苏宁云商集团股份有限公司</span>
               </span>
-              <span>2017 年 11 月 - 2018 年 4 月</span>
+                <span>2017 年 11 月 - 2018 年 4 月</span>
+              </div>
+              <div class="department">
+                <span>苏宁技术研究院 - 数据分析部</span>
+                <span>前端工程师</span>
+              </div>
             </div>
-            <div class="department">
-              <span>苏宁技术研究院 - 数据分析部</span>
-              <span>前端工程师</span>
+            <div class="content-description">
+              <span>负责集团数据可视化分析的前端工作，使苏宁线下线上促销活动的数据更加直观。</span>
             </div>
           </div>
-          <div class="content-description">
-            <span>负责集团数据可视化分析的前端工作，使苏宁线下线上促销活动的数据更加直观。</span>
+        </div>
+        <!--邦宁-->
+        <div class="resume-experience">
+          <div class="resume-experience-timeline">
+            <div class="timeline-line-before"></div>
+            <div class="timeline-circle"></div>
+            <div class="timeline-line"></div>
           </div>
-        </div>
-      </div>
-      <!--邦宁-->
-      <div class="resume-experience">
-        <div class="resume-experience-timeline">
-          <div class="timeline-line-before"></div>
-          <div class="timeline-circle"></div>
-          <div class="timeline-line"></div>
-        </div>
-        <div class="resume-experience-content">
-          <div class="content-title">
-            <div class="company">
+          <div class="resume-experience-content">
+            <div class="content-title">
+              <div class="company">
               <span class="company-name">
                 <div class="company-logo-bangning">
                   <i class="icon-company"></i>
                 </div>
                 <span>江苏邦宁科技有限公司</span>
               </span>
-              <span>2016 年 6 月 - 2017 年 8 月</span>
+                <span>2016 年 6 月 - 2017 年 8 月</span>
+              </div>
+              <div class="department">
+                <span>主站研发部</span>
+                <span>ASP工程师</span>
+              </div>
             </div>
-            <div class="department">
-              <span>主站研发部</span>
-              <span>ASP工程师</span>
+            <div class="content-description">
+              <span>参与公司销售网站从后端到前端的开发和维护；收集需求开发管理系统提高客服工作效率；管理服务器，承担一部分的运维工作。</span>
             </div>
-          </div>
-          <div class="content-description">
-            <span>参与公司销售网站从后端到前端的开发和维护；收集需求开发管理系统提高客服工作效率；管理服务器，承担一部分的运维工作。</span>
           </div>
         </div>
       </div>
-    </div>
-    <!--项目经验-->
-    <div class="resume-item">
-      <h3>项目经验</h3>
-      <div class="resume-experience" v-for="(project, index) in resume.projects" :key="`resume-experience-${index}`">
-        <div class="resume-experience-timeline">
-          <div class="timeline-line-before"></div>
-          <div class="timeline-circle"></div>
-          <div class="timeline-line"></div>
-        </div>
-        <div class="resume-experience-content">
-          <div class="content-title">
-            <div class="project">
+      <!--项目经验-->
+      <div class="resume-item">
+        <h3>项目经验</h3>
+        <div class="resume-experience" v-for="(project, index) in resume.projects" :key="`resume-experience-${index}`">
+          <div class="resume-experience-timeline">
+            <div class="timeline-line-before"></div>
+            <div class="timeline-circle"></div>
+            <div class="timeline-line"></div>
+          </div>
+          <div class="resume-experience-content">
+            <div class="content-title">
+              <div class="project">
               <span class="project-name">
                 <span>{{ project.name }}</span>
                 <span class="project-badges">
@@ -141,46 +142,47 @@
                   <!--<b-badge pill variant="info" class="project-badge">管理</b-badge>-->
                 </span>
               </span>
-              <span>{{ project.timeRange }}</span>
+                <span>{{ project.timeRange }}</span>
+              </div>
+            </div>
+            <div class="project-detail" v-for="(detail, index) in project.detail" :key="`project-detail-${index}`">
+              <div class="detail-name">{{ detail.name }}：</div>
+              <div class="detail-content">{{ detail.value }}</div>
             </div>
           </div>
-          <div class="project-detail" v-for="(detail, index) in project.detail" :key="`project-detail-${index}`">
-            <div class="detail-name">{{ detail.name }}：</div>
-            <div class="detail-content">{{ detail.value }}</div>
+        </div>
+      </div>
+      <!--技能-->
+      <div class="resume-item">
+        <h3>技能</h3>
+        <div class="skill-show">
+          <div class="skill-item" v-for="(skill, index) in resume.skills" :key="`skill-item-${index}`">
+            <span class="skill-name">{{ skill.name }}</span>
+            <b-progress
+              :value="skill.level"
+              height="12px"
+              class="skill-progress"></b-progress>
           </div>
         </div>
       </div>
-    </div>
-    <!--技能-->
-    <div class="resume-item">
-      <h3>技能</h3>
-      <div class="skill-show">
-        <div class="skill-item" v-for="(skill, index) in resume.skills" :key="`skill-item-${index}`">
-          <span class="skill-name">{{ skill.name }}</span>
-          <b-progress
-            :value="skill.level"
-            height="12px"
-            class="skill-progress"></b-progress>
+      <!--专利-->
+      <div class="resume-item">
+        <h3>专利</h3>
+        <div class="patent">
+          <div class="patent-item">
+            <img class="icon-html5" src="../assets/img/html5.png"/>
+            <span>基于HTML5协议的浏览器全屏显示方法及显示装置[WHD18002（PT20180080-DD-P）]</span>
+          </div>
         </div>
       </div>
-    </div>
-    <!--专利-->
-    <div class="resume-item">
-      <h3>专利</h3>
-      <div class="patent">
-        <div class="patent-item">
-          <img class="icon-html5" src="../assets/img/html5.png"/>
-          <span>基于HTML5协议的浏览器全屏显示方法及显示装置[WHD18002（PT20180080-DD-P）]</span>
+      <!--底部-->
+      <div class="resume-footer">
+        <div class="footer-line"></div>
+        <div class="footer-content">
+          <span>Made with 💖 by Mazey（End）</span>
         </div>
+        <div class="footer-line"></div>
       </div>
-    </div>
-    <!--底部-->
-    <div class="resume-footer">
-      <div class="footer-line"></div>
-      <div class="footer-content">
-        <span>Made with 💖 by Mazey（End）</span>
-      </div>
-      <div class="footer-line"></div>
     </div>
   </div>
 </template>
