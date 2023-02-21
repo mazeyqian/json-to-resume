7653dc3f578d110427c8cf4681d38e24cd1211f2
Mazey Chu <mazey@mazey.net>
2023-02-21 12:07:19 +0800

    feat: optimize the records

--- src/components/m-home.vue
+++ src/components/m-home.vue
@@ -2,6 +2,6 @@
   <div :style="{ padding: '4px' }">
     <p><img src="../assets/img/home-bgc-640x360-tiny.jpg" alt="夏目友人帐"></p>
     <p>欢迎你来到我的网站~</p>
-    <p>这是我的备案号：<a href="http://beian.miit.gov.cn/">苏ICP备16050427号</a></p>
+    <p>这是我的备案号：<a href="https://beian.miit.gov.cn/">苏ICP备16050427号-3</a></p>
   </div>
 </template>
