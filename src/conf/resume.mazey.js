9c53ce3a3d6c46d9b6ddba520bcd98a6a0c43878
Mazey Chu <mazey@mazey.net>
2023-03-11 17:02:36 +0800

    feat: optimize the logic of others' module

---
+++ src/conf/resume.mazey.js
@@ -0,0 +1,704 @@
+export default {
+  title: '前端工程师',
+  pageAndFileName: '钱程-前端工程师-6年',
+  personInfo: {
+    left: [
+      {
+        name: '姓名',
+        value: '钱程',
+      },
+      {
+        name: '性别',
+        value: '男',
+      },
+      {
+        name: '现居',
+        value: '上海',
+      },
+      {
+        name: '出生',
+        value: '1993 年 3 月',
+      },
+      {
+        name: '电话',
+        value: '18551470413',
+      },
+      {
+        name: '邮箱',
+        value: `<a href="mailto:mazeyqian@gmail.com" rel="noopener">mazeyqian@gmail.com</a>`,
+      },
+      {
+        name: '工作年限',
+        value: '6 年',
+      },
+    ],
+    right: [
+      {
+        name: '院校',
+        value: `三江学院<span class="resume-small">（2012 年 9 月 - 2016 年 6 月）</span>`,
+      },
+      {
+        name: '专业',
+        value: '电气工程及其自动化',
+      },
+      {
+        name: '学历',
+        value: '本科',
+      },
+      {
+        name: '英语',
+        value: 'CET-6',
+      },
+      {
+        name: '博客',
+        value: `<a href="https://blog.mazey.net/" target="_blank" rel="noopener">blog.mazey.net</a>`,
+      },
+      {
+        name: 'GitHub',
+        value: `<a href="https://github.com/mazeyqian" target="_blank" rel="noopener">github.com/mazeyqian</a>`,
+      },
+      {
+        name: '站酷',
+        value: `<a href="https://www.zcool.com.cn/u/24032804" target="_blank" rel="noopener">zcool.com.cn/u/24032804</a>`,
+      },
+    ],
+  },
+  companies: [
+    {
+      name: '上海哔哩哔哩科技有限公司',
+      iconName: 'bilibili',
+      timeRangeStart: '2022 年 5 月',
+      timeRangeEnd: '至今',
+      tags: [
+        {
+          name: '手机游戏',
+          bgc: 'game'
+        },
+        {
+          name: '发行',
+          bgc: 'publish'
+        },
+      ],
+      department: '游戏发行事业部 - 游戏业务组',
+      position: '前端工程师',
+      description: '哔哩哔哩游戏中心桌面端 Web 和移动端 H5，维护面向外部游戏厂商的开放平台和为内部运营服务的管理后台。',
+      products: [
+        {
+          name: '黑猫警长',
+          iconName: 'mr-black',
+          link: 'https://example.com',
+          bgc: 'black',
+        },
+        {
+          name: '哆啦A梦',
+          iconName: 'doraemon',
+          link: 'https://example.com',
+          bgc: 'cyan',
+        },
+        {
+          name: '猫和老鼠',
+          iconName: 'tom',
+          link: 'https://example.com',
+          bgc: 'secondary',
+        },
+      ],
+    },
+    {
+      name: '上海莉莉丝科技股份有限公司',
+      iconName: 'lilith',
+      timeRangeStart: '2020 年 8 月',
+      timeRangeEnd: '2022 年 4 月',
+      tags: [
+        {
+          name: '手机游戏',
+          bgc: 'game'
+        },
+        {
+          name: '发行',
+          bgc: 'publish'
+        },
+      ],
+      department: '平台产品中心 - 游戏组件组',
+      position: '前端&Node.js工程师',
+      description: '搭建游戏礼包兑换码、会员中心，维护游戏官网、社区、支付系统。',
+      products: [
+        {
+          name: '旅行拍照',
+          iconName: 'pet-photography',
+          link: 'https://example.com',
+          bgc: 'vue',
+        },
+        {
+          name: '布拉格猫咪咖啡馆',
+          iconName: 'cat-sale',
+          link: 'https://example.com',
+          bgc: 'coffee',
+        },
+      ],
+    },
+    {
+      name: '上海基分文化传播有限公司',
+      iconName: 'qtt',
+      timeRangeStart: '2018 年 12 月',
+      timeRangeEnd: '2020 年 8 月',
+      tags: [
+        {
+          name: '新闻资讯',
+          bgc: 'blue'
+        },
+        {
+          name: '广告',
+          bgc: 'a-d'
+        },
+        {
+          name: '发行',
+          bgc: 'publish'
+        },
+      ],
+      department: '技术中心 - 游戏组',
+      position: '前端&Golang工程师',
+      description: '搭建集成在趣头条 App 中的游戏中心入口落地页，以及提供面向外部游戏厂商的 JS-SDK。',
+    },
+    {
+      name: '平纳科信息咨询有限公司',
+      iconName: 'pinnacle',
+      timeRangeStart: '2018 年 7 月',
+      timeRangeEnd: '2018 年 12 月',
+      tags: [
+        {
+          name: '招聘',
+          bgc: 'recruit'
+        },
+        {
+          name: '智能推荐',
+          bgc: 'cyan'
+        },
+        {
+          name: 'SaaS',
+          bgc: 'saas'
+        },
+      ],
+      department: '技术部',
+      position: '前端工程师',
+      description: '负责公司招聘系统的前端开发，完善招聘工作流；承担前端项目管理和运维的工作。',
+    },
+    {
+      name: '苏宁云商集团股份有限公司',
+      iconName: 'suning',
+      timeRangeStart: '2017 年 11 月',
+      timeRangeEnd: '2018 年 4 月',
+      tags: [
+        {
+          name: '电商',
+          bgc: 'e-commerce'
+        },
+      ],
+      department: '苏宁技术研究院 - 数据分析部',
+      position: '前端工程师',
+      description: '负责集团数据可视化分析的前端工作，使苏宁易购线下线上促销活动的数据更加直观。',
+    },
+    {
+      name: '江苏邦宁科技有限公司',
+      iconName: 'bangning',
+      timeRangeStart: '2016 年 6 月',
+      timeRangeEnd: '2017 年 8 月',
+      tags: [
+        {
+          name: '域名注册',
+          bgc: 'domain-register'
+        },
+        {
+          name: '云主机',
+          bgc: 'cloud-host'
+        },
+        {
+          name: '智能建站',
+          bgc: 'auto-web'
+        },
+      ],
+      department: '主站研发部',
+      position: 'PHP工程师',
+      description: '参与公司域名售卖网站（nic.top）从后端到前端的开发和维护；承担服务器运维工作。',
+    },
+  ],
+  _projects: [/* Disable */],
+  projects: [
+    {
+      name: '莉莉丝游戏内社区',
+      timeRangeStart: '2020 年 10 月',
+      timeRangeEnd: '2022 年 4 月',
+      tags: [
+        {
+          name: 'ToC',
+          bgc: 'cyan'
+        },
+        {
+          name: '国际化',
+          bgc: 'blue'
+        }
+      ],
+      details: [
+        {
+          name: '前端技术',
+          value: 'React、Umi、Less、SSR'
+        },
+        {
+          name: '项目描述',
+          value: `社区覆盖 AFK Arena、Rise of Kingdoms、Warpath 等游戏的国服及国际服环境，具备不同人群、语言环境的社区讨论及运营支持能力。`
+        }
+      ],
+      hideInPrint: true,
+    },
+    {
+      name: '莉莉丝游戏（万国觉醒&剑与远征）会员中心',
+      timeRangeStart: '2020 年 8 月',
+      timeRangeEnd: '2021 年 3 月',
+      tags: [
+        {
+          name: 'ToC',
+          bgc: 'cyan'
+        },
+        {
+          name: 'CI/CD',
+          bgc: 'gitlab'
+        }
+      ],
+      details: [
+        {
+          name: '前端技术',
+          value: 'React、Webpack、Sass'
+        },
+        {
+          name: '后端技术',
+          value: 'Node.js、Egg.js、Redis、MySQL、Sequelize、Docker、Nginx'
+        },
+        {
+          name: '项目描述',
+          value: `承载玩家会员信息展示和礼包中心，访问地址：<a href="https://account.lilith.com" target="_blank" rel="noopener">https://account.lilith.com</a>`
+        }
+      ]
+    },
+    {
+      name: '莉莉丝游戏（万国觉醒&剑与远征）礼包兑换码（CDKey）网站',
+      timeRangeStart: '2020 年 9 月',
+      timeRangeEnd: '2021 年 3 月',
+      tags: [
+        {
+          name: 'ToC',
+          bgc: 'cyan'
+        },
+        {
+          name: '国际化',
+          bgc: 'blue'
+        }
+      ],
+      details: [
+        {
+          name: '前端技术',
+          value: 'React、Umi、Less、SSR'
+        },
+        {
+          name: '后端技术',
+          value: 'Node.js、Egg.js、Redis、Nginx'
+        },
+        {
+          name: '项目描述',
+          value: `搭建日服礼包兑换码网站，并迭代至国际化，访问地址：<a href="https://cdkey.lilith.com/afk-global" target="_blank" rel="noopener">https://cdkey.lilith.com/afk-global</a>`
+        }
+      ]
+    },
+    {
+      name: '趣头条游戏中心 JS-SDK',
+      timeRangeStart: '2019 年 2 月',
+      timeRangeEnd: '2020 年 7 月',
+      tags: [
+        {
+          name: '支付',
+          bgc: 'pay'
+        },
+        {
+          name: '广告',
+          bgc: 'a-d'
+        },
+        {
+          name: '埋点',
+          bgc: 'blue'
+        },
+        {
+          name: '分享',
+          bgc: 'green'
+        },
+        {
+          name: 'API',
+          bgc: 'api'
+        },
+        {
+          name: '错误监控',
+          bgc: 'error-monitor'
+        },
+        {
+          name: '灰度发版',
+          bgc: 'secondary'
+        }
+      ],
+      details: [
+        {
+          name: '前端技术',
+          value: 'Webpack、Rollup、Gulp、Docsify、Sass'
+        },
+        {
+          name: '后端技术',
+          value: 'Golang、Gin、Gorm、MySQL、MurmurHash、JsonLogic'
+        },
+        {
+          name: '项目描述',
+          value: `给游戏厂商提供封装好的支付、广告、游戏任务、返回劫持、数据上报、分享以及调用原生 App 功能的能力，游戏 SDK 文档：<a href="https://mazey.cn/t/dl" target="_blank" rel="noopener">mazey.cn/t/dl</a>`
+        }
+      ]
+    },
+    {
+      name: '游戏中心 CMS 后台 - 广告配置',
+      timeRangeStart: '2019 年 12 月',
+      timeRangeEnd: '2020 年 1 月',
+      tags: [
+        {
+          name: 'ToA',
+          bgc: 'cyan'
+        },
+        {
+          name: '效率',
+          bgc: 'green'
+        }
+      ],
+      details: [
+        {
+          name: '前端技术',
+          value: 'Vue 2、Element-UI'
+        },
+        {
+          name: '项目描述',
+          value: `随着游戏体量的增加和单平台向多平台切换的背景下，早期业务的广告全局配置已然不能满足商务工作需求，存在低效易错的问题。
+          通过 CMS 后台的广告配置，单个平台只需要配置一次广告配置，为商务每天<b>节约 1~2 小时</b>的工作时间。`
+        }
+      ]
+    },
+    {
+      name: '游戏中心开放平台 - 游戏版本和灰度控制',
+      timeRangeStart: '2019 年 8 月',
+      timeRangeEnd: '2019 年 11 月',
+      tags: [
+        {
+          name: 'ToB',
+          bgc: 'cyan'
+        },
+        {
+          name: '灰度发版',
+          bgc: 'secondary'
+        }
+      ],
+      details: [
+        {
+          name: '前端技术',
+          value: 'Vue 2、Element-UI'
+        },
+        {
+          name: '项目描述',
+          value: `打通游戏测试、灰度、发布流程，重点游戏做版本更新的时候，先放量一部分用户体验游戏，观察数据和客诉没问题之后再全量，从而保证头部游戏（20+）的稳定性，节约了运营大量的验收成本。`
+        }
+      ],
+      hideInPrint: true,
+    },
+    {
+      name: '趣头条游戏中心首页 - 加载体验优化',
+      timeRangeStart: '2019 年 6 月 10 日',
+      timeRangeEnd: '2019 年 6 月 25 日',
+      tags: [
+        {
+          name: '性能',
+          bgc: 'orange'
+        },
+        {
+          name: '用户体验',
+          bgc: 'green'
+        }
+      ],
+      details: [
+        {
+          name: '项目描述',
+          value: `由于早期的业务快速迭代，游戏中心 H5 页面存在加载慢体验差的问题，为了提升页面转化率和用户体验，对首页进行了一次短平快的优化，首屏时间从 3357ms <b>减少到 917ms（-72%）</b>，页面折损率<b>降低 3%</b>，优化过程：<a href="https://mazey.cn/t/fh" target="_blank" rel="noopener">mazey.cn/t/fh</a>`
+        }
+      ]
+    },
+    {
+      name: 'PinnacleSolution 智能招聘系统',
+      timeRangeStart: '2018 年 7 月',
+      timeRangeEnd: '2018 年 12 月',
+      tags: [
+        {
+          name: 'CRM',
+          bgc: 'crm'
+        },
+        {
+          name: '效率',
+          bgc: 'green'
+        }
+      ],
+      details: [
+        {
+          name: '前端技术',
+          value: 'Vue 2、Vue-cli 3、ColorAdmin、Echarts 4、Sass'
+        },
+        {
+          name: '项目描述',
+          value: `参与前期需求确认到项目稳定上线的整个过程；将客户经理和招聘专员的工作进行分离；整合搜索、筛选、沟通、确认模块，使招聘专员能够一站式的进行管理和操作。`
+        }
+      ]
+    },
+    {
+      name: 'IDAP 智能数据分析平台',
+      timeRangeStart: '2017 年 11 月',
+      timeRangeEnd: '2018 年 4 月',
+      tags: [
+        {
+          name: '数据可视化',
+          bgc: 'blue'
+        },
+        {
+          name: '大屏',
+          bgc: 'orange'
+        },
+        {
+          name: '响应式',
+          bgc: 'purple'
+        }
+      ],
+      details: [
+        {
+          name: '前端技术',
+          value: 'Vue 2、Element-UI、Echarts 3、Webpack、Sass'
+        },
+        {
+          name: '项目描述',
+          value: '项目分为实时数据展示、历史数据分析、促销活动后台管理三个部分；在苏宁年货节、418 等大促活动中承担数据监控和分析的作用，其中大屏模块在活动期间一直投放在公司公众视野中，使全体员工了解大促进展。'
+        }
+      ]
+    },
+    {
+      name: '邦宁域名备案核查系统',
+      timeRangeStart: '2017 月 4 月',
+      timeRangeEnd: '2017 年 5 月',
+      tags: [
+        {
+          name: 'CRM',
+          bgc: 'crm'
+        },
+        {
+          name: 'IE8+',
+          bgc: 'ms'
+        },
+        {
+          name: '效率',
+          bgc: 'green'
+        },
+        {
+          name: '报表',
+          bgc: 'secondary'
+        }
+      ],
+      details: [
+        {
+          name: '前端技术',
+          value: 'Bootstrap、jQuery、Less'
+        },
+        {
+          name: '后端技术',
+          value: 'WindowsServer、IIS、ASP、SQLServer'
+        },
+        {
+          name: '项目描述',
+          value: '前期收集需求，调查用户喜欢的体验；设计项目流程，数据库结构；独立完成后端到前端的开发；保证项目线上的稳定运行。通过数据筛选、数据对比、批量处理数据等功能提高备案专员的工作效率。'
+        }
+      ],
+      hideInPrint: true,
+    },
+  ],
+  _individualProjects: [/* Disable */],
+  individualProjects: [
+    {
+      name: '小栗读书会',
+      iconLink: '//blog.mazey.net/wp-content/uploads/2022/03/gray-6ce2b956-144x144-1.png',
+      tags: [
+        {
+          name: 'Web',
+          bgc: 'blue'
+        },
+        {
+          name: '分享',
+          bgc: 'green'
+        },
+        {
+          name: 'PWA',
+          bgc: 'purple'
+        },
+      ],
+      details: [
+        {
+          name: '项目描述',
+          value: `提供一个读书分享的平台，访问地址：<a href="https://mazey.cn/t/fc" target="_blank" rel="noopener">mazey.cn/t/fc</a>`
+        }
+      ]
+    },
+    {
+      name: '上海地铁运营系统',
+      iconLink: '//blog.mazey.net/wp-content/uploads/2022/03/metro-144x144-1.png',
+      tags: [
+        {
+          name: 'Web',
+          bgc: 'blue'
+        },
+        {
+          name: '数据可视化',
+          bgc: 'blue'
+        },
+        {
+          name: 'D3.js',
+          bgc: 'd3'
+        },
+      ],
+      details: [
+        {
+          name: '项目描述',
+          value: `上海地铁列车实时运行系统，DEMO 地址：<a href="https://mazey.cn/t/fe" target="_blank" rel="noopener">mazey.cn/t/fe</a>`
+        }
+      ]
+    },
+    {
+      name: 'Mazey',
+      iconLink: '//blog.mazey.net/wp-content/uploads/2022/03/npm-icon-144x144-1.png',
+      tags: [
+        {
+          name: 'NPM',
+          bgc: 'npm'
+        },
+        {
+          name: '工具',
+          bgc: 'secondary'
+        },
+      ],
+      details: [
+        {
+          name: '项目描述',
+          value: `前端函数库，访问地址：<a href="https://github.com/mazeyqian/mazey" target="_blank" rel="noopener">https://github.com/mazeyqian/mazey</a>`
+        }
+      ]
+    },
+    {
+      name: 'AliyunOss-Cli',
+      iconLink: '//blog.mazey.net/wp-content/uploads/2022/03/npm-icon-144x144-1.png',
+      tags: [
+        {
+          name: 'NPM',
+          bgc: 'npm'
+        },
+        {
+          name: 'CI/CD',
+          bgc: 'gitlab'
+        }
+      ],
+      details: [
+        {
+          name: '项目描述',
+          value: `阿里云 OSS 文件上传 CLI，访问地址：<a href="https://github.com/mazeyqian/aliyunoss-cli" target="_blank" rel="noopener">https://github.com/mazeyqian/aliyunoss-cli</a>`
+        }
+      ]
+    },
+    {
+      name: '短链接/短网址在线生成',
+      iconLink: '//blog.mazey.net/wp-content/uploads/2022/03/logo-dark-circle-144x144-1.png',
+      tags: [
+        {
+          name: 'Web',
+          bgc: 'blue'
+        },
+      ],
+      details: [
+        {
+          name: '项目描述',
+          value: `长链接转换成短链接，访问地址：<a href="https://blog.mazey.net/tiny" target="_blank" rel="noopener">https://blog.mazey.net/tiny</a>`
+        }
+      ]
+    },
+    {
+      name: '前端工作小站',
+      iconLink: '//blog.mazey.net/wp-content/uploads/2022/03/logo-dark-circle-144x144-1.png',
+      tags: [
+        {
+          name: 'Web',
+          bgc: 'blue'
+        },
+        {
+          name: '数据可视化',
+          bgc: 'blue'
+        },
+        {
+          name: 'ToB',
+          bgc: 'cyan'
+        },
+        {
+          name: '效率',
+          bgc: 'green'
+        },
+      ],
+      details: [
+        {
+          name: '项目描述',
+          value: `前端性能监控，静态资源上传，DEMO 地址：<a href="https://mazey.cn/t/fg" target="_blank" rel="noopener">mazey.cn/t/fg</a>，密码：demo`
+        }
+      ]
+    },
+  ],
+  _skills: [/* Disable */],
+  skills: [
+    {
+      name: 'JavaScript',
+      level: 95
+    },
+    {
+      name: 'Golang',
+      level: 90
+    },
+    {
+      name: 'React/Vue',
+      level: 85
+    },
+    {
+      name: 'CSS',
+      level: 80
+    },
+    {
+      name: 'Sass/Less',
+      level: 75
+    },
+    {
+      name: 'Node.js',
+      level: 70
+    },
+    {
+      name: 'Sketch/PS',
+      level: 70
+    },
+    {
+      name: '墨刀',
+      level: 70
+    }
+  ],
+  _others: [/* Disable */],
+  others: [
+    {
+      name: '专利：基于HTML5协议的浏览器全屏显示方法及显示装置',
+      iconName: 'html5',
+      linkContent: 'CN110083409A',
+      link: '//i.mazey.net/static/patent/CN102018000076427CN00001100834090APDFZH20190802CN00N.PDF',
+    },
+  ],
+}
