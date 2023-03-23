d6a2e24bb1b1d960e8d04458f9141cd8ac45c04a
Mazey Chu <mazey@mazey.net>
2023-03-23 17:07:25 +0800

    docs: optimize description

--- src/conf/resume.mazey.js
+++ src/conf/resume.mazey.js
@@ -105,7 +105,7 @@ export default {
           云游戏管理平台：支撑云游戏能力，提供云游戏内时长控制、包管理、厂商接入等功能
         </li>
         <li class="list-group-item">
-          毕方社区：<a href="https://www.bigfun.cn/tools/pcrteam/" target="_blank" rel="noopener">公主连结团队战工具</a>、<a href="https://www.bigfun.cn/tools/pcr/" target="_blank" rel="noopener">公主连结工具站</a>、<a href="https://www.bigfun.cn/tools/gt/" target="_blank" rel="noopener">坎公百宝袋</a>
+          毕方社区游戏工具：<a href="https://www.bigfun.cn/tools/pcrteam/" target="_blank" rel="noopener">公主连结团队战工具</a>、<a href="https://www.bigfun.cn/tools/pcr/" target="_blank" rel="noopener">公主连结工具站</a>、<a href="https://www.bigfun.cn/tools/gt/" target="_blank" rel="noopener">坎公百宝袋</a>
         </li>
       </ul>`,
       products: [
